a254d2d85910cf54a5a48c66e1c4fc6b950defd8
marcelooliveira <mclricardo@gmail.com>
2018-07-16 17:46:24 -0300

    .

--- Topico5.Comparações/Program.cs
+++ Topico5.Comparações/Program.cs
@@ -31,73 +31,63 @@ namespace Topico5
             Console.WriteLine(aluno1.Equals(aluno2));
             Console.WriteLine(aluno1.Equals(aluno3));
 
-            //var aluno4 = new Aluno()
-            //{
-            //    Nome = "ANDRÉ DOS SANTOS",
-            //    DataNascimento = new DateTime(1970, 1, 1)
-            //};
+            var aluno4 = new Aluno()
+            {
+                Nome = "ANDRÉ DOS SANTOS",
+                DataNascimento = new DateTime(1970, 1, 1)
+            };
 
-            //var aluno5 = new Aluno()
-            //{
-            //    Nome = "Ana de Souza",
-            //    DataNascimento = new DateTime(1990, 1, 1)
-            //};
+            var aluno5 = new Aluno()
+            {
+                Nome = "Ana de Souza",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
 
-            //Console.WriteLine(aluno1);
-            //Console.WriteLine(aluno2);
-            //Console.WriteLine(aluno3);
-            //Console.WriteLine(aluno4);
-            //Console.WriteLine(aluno5);
+            Console.WriteLine(aluno1);
+            Console.WriteLine(aluno2);
+            Console.WriteLine(aluno3);
+            Console.WriteLine(aluno4);
+            Console.WriteLine(aluno5);
 
-            //var alunos = new List<Aluno>
-            //    { aluno1, aluno2, aluno3, aluno4, aluno5};
+            var alunos = new List<Aluno>
+                { aluno1, aluno2, aluno3, aluno4, aluno5};
 
-            //foreach (var aluno in alunos)
-            //{
-            //    Console.WriteLine(aluno);
-            //}
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
 
-            //alunos.Sort();
+            alunos.Sort();
 
-            //foreach (var aluno in alunos)
-            //{
-            //    Console.WriteLine(aluno);
-            //}
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
 
             Console.ReadKey();
         }
     }
 
-    class Aluno
-    {
-        public string Nome { get; set; }
-        public DateTime DataNascimento { get; set; }
-
-        public override string ToString()
-        {
-            return $"Nome: {Nome}, Nascimento: {DataNascimento}";
-        }
-    }
-
-
-
-
-
-    //class Aluno : IComparable
+    //class Aluno
     //{
     //    public string Nome { get; set; }
     //    public DateTime DataNascimento { get; set; }
 
-    //    public int CompareTo(object obj)
+    //    public override string ToString()
     //    {
-    //        Aluno outro = obj as Aluno;
-    //        int resultado = this.DataNascimento.CompareTo(outro.DataNascimento);
-    //        if (resultado == 0)
-    //        {
-    //            resultado = this.Nome.CompareTo(outro.Nome);
-    //        }
+    //        return $"Nome: {Nome}, Nascimento: {DataNascimento}";
+    //    }
+    //}
 
-    //        return resultado;
+
+    //class Aluno
+    //{
+    //    public string Nome { get; set; }
+    //    public DateTime DataNascimento { get; set; }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Nome: {Nome}, Nascimento: {DataNascimento}";
     //    }
 
     //    public override bool Equals(object obj)
@@ -108,18 +98,91 @@ namespace Topico5
     //            return false;
     //        }
 
-    //        //return outro.Nome == this.Nome;
+    //        return outro.Nome.Equals(this.Nome);
+    //    }
+    //}
 
-    //        //return outro.Nome.Equals(this.Nome, StringComparison.InvariantCultureIgnoreCase);
+    //class Aluno
+    //{
+    //    public string Nome { get; set; }
+    //    public DateTime DataNascimento { get; set; }
 
-    //        return outro.Nome.Equals(this.Nome, StringComparison.InvariantCultureIgnoreCase)
+    //    public override string ToString()
+    //    {
+    //        return $"Nome: {Nome}, Nascimento: {DataNascimento}";
+    //    }
+
+    //    public override bool Equals(object obj)
+    //    {
+    //        Aluno outro = obj as Aluno;
+    //        if (outro == null)
+    //        {   
+    //            return false;
+    //        }
+
+    //        return outro.Nome.Equals(this.Nome, StringComparison.CurrentCultureIgnoreCase);
+    //    }
+
+    //    public override int GetHashCode()
+    //    {
+    //        return this.Nome.GetHashCode();
+    //    }
+    //}
+
+    //class Aluno
+    //{
+    //    public string Nome { get; set; }
+    //    public DateTime DataNascimento { get; set; }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Nome: {Nome}, Nascimento: {DataNascimento}";
+    //    }
+
+    //    public override bool Equals(object obj)
+    //    {
+    //        Aluno outro = obj as Aluno;
+    //        if (outro == null)
+    //        {
+    //            return false;
+    //        }
+
+    //        return outro.Nome.Equals(this.Nome, StringComparison.CurrentCultureIgnoreCase)
     //            && outro.DataNascimento.Equals(this.DataNascimento);
     //    }
 
-    //    //public override int GetHashCode()
-    //    //{
-    //    //    return this.Nome.GetHashCode();
-    //    //}
+    //    public override int GetHashCode()
+    //    {
+    //        var hashCode = -1523756618;
+    //        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+    //        hashCode = hashCode * -1521134295 + DataNascimento.GetHashCode();
+    //        return hashCode;
+    //    }
+    //}
+
+
+
+    //class Aluno : IComparable
+    //{
+    //    public string Nome { get; set; }
+    //    public DateTime DataNascimento { get; set; }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Nome: {Nome}, Nascimento: {DataNascimento}";
+    //    }
+
+    //    public override bool Equals(object obj)
+    //    {
+    //        Aluno outro = obj as Aluno;
+    //        if (outro == null)
+    //        {
+    //            return false;
+    //        }
+
+    //        return outro.Nome.Equals(this.Nome, StringComparison.CurrentCultureIgnoreCase)
+    //            && outro.DataNascimento.Equals(this.DataNascimento);
+    //    }
 
     //    public override int GetHashCode()
     //    {
@@ -129,11 +192,57 @@ namespace Topico5
     //        return hashCode;
     //    }
 
-    //    public override string ToString()
+    //    public int CompareTo(object obj)
     //    {
-    //        return $"Nome: {Nome}, Nascimento: {DataNascimento}";
+    //        Aluno outro = obj as Aluno;
+    //        return this.DataNascimento.CompareTo(outro.DataNascimento);
     //    }
     //}
+
+
+
+    class Aluno : IComparable
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Nascimento: {DataNascimento}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outro = obj as Aluno;
+            if (outro == null)
+            {
+                return false;
+            }
+
+            return outro.Nome.Equals(this.Nome, StringComparison.CurrentCultureIgnoreCase)
+                && outro.DataNascimento.Equals(this.DataNascimento);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1523756618;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + DataNascimento.GetHashCode();
+            return hashCode;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Aluno outro = obj as Aluno;
+            int resultado = this.DataNascimento.CompareTo(outro.DataNascimento);
+            if (resultado == 0)
+            {
+                resultado = this.Nome.CompareTo(outro.Nome);
+            }
+
+            return resultado;
+        }
+    }
 }
 
 
